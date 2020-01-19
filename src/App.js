7fcc9e6d8e0e893f9c494cbf1cb41d8af7b3f19b
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-19 23:05:20 +0100

    archived Coin Flip project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import HeadFliper from './components/headfliper/HeadFliper';
 
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Let us flip coin</h1>
-      <HeadFliper />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
