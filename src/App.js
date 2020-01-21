78b7965e9fb5d5d4ab33fade79e66f510dce21a0
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-21 18:38:45 +0100

    added initial set-up code for hang-man project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Hangman from "./Hangman";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Hangman />
       </div>
     );
   }
