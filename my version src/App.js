e3944093381117b5ac2188c8dcee7f089975116b
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-23 17:26:50 +0100

    compared the code with Colt's

---
+++ my version src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './App.css';
+import TodoList from './TodoList';
+import NewTodoForm from './NewTodoForm';
+import uuid from 'uuid';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [{ id: uuid(), task: 'take children from home' }]
+    };
+    this.addTodo = this.addTodo.bind(this);
+  }
+  addTodo(item) {
+    this.setState({
+      todos: [
+        ...this.state.todos,
+        {
+          task: item.todoField,
+          id: uuid()
+        }
+      ]
+    });
+  }
+  Update(){
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <TodoList todos={this.state.todos} Update={this.Update} />
+        <NewTodoForm addTodo={this.addTodo} />
+      </div>
+    );
+  }
+}
+
+export default App;
