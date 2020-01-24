fbf97b8c53fc0407faa047c2bdb10309f1d4e271
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-24 19:02:36 +0100

    archive api exercise project

---
+++ Previous Projects/API Exercise/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ListOfCards from './ListOfCards';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ListOfCards />
+      </div>
+    );
+  }
+}
+
+export default App;
