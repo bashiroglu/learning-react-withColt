ca3f7eb9067bff94dc219e9398d1d6a372675f55
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-24 23:36:01 +0100

    add commnet as notes to  Colt's code

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import Joke from "./Joke";
-import axios from "axios";
-import uuid from "uuid/v4";
-import "./JokeList.css";
+import React, { Component } from 'react';
+import Joke from './Joke';
+import axios from 'axios';
+import uuid from 'uuid/v4';
+import './JokeList.css';
 
 class JokeList extends Component {
   static defaultProps = {
@@ -11,10 +11,12 @@ class JokeList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]"),
+      jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]'),
       loading: false
     };
-    this.seenJokes = new Set(this.state.jokes.map(j => j.text));
+    this.seenJokes = new Set(
+      this.state.jokes.map(j => j.text)
+    ); /* we do this because of jokes comes from local storage */
     console.log(this.seenJokes);
     this.handleClick = this.handleClick.bind(this);
   }
@@ -25,14 +27,14 @@ class JokeList extends Component {
     try {
       let jokes = [];
       while (jokes.length < this.props.numJokesToGet) {
-        let res = await axios.get("https://icanhazdadjoke.com/", {
-          headers: { Accept: "application/json" }
+        let res = await axios.get('https://icanhazdadjoke.com/', {
+          headers: { Accept: 'application/json' }
         });
         let newJoke = res.data.joke;
         if (!this.seenJokes.has(newJoke)) {
           jokes.push({ id: uuid(), text: newJoke, votes: 0 });
         } else {
-          console.log("FOUND A DUPLICATE!");
+          console.log('FOUND A DUPLICATE!');
           console.log(newJoke);
         }
       }
@@ -42,7 +44,7 @@ class JokeList extends Component {
           jokes: [...st.jokes, ...jokes]
         }),
         () =>
-          window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+          window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
       );
     } catch (e) {
       alert(e);
@@ -50,6 +52,7 @@ class JokeList extends Component {
     }
   }
   handleVote(id, delta) {
+    /*  delta help us whether to increase or decrease */
     this.setState(
       st => ({
         jokes: st.jokes.map(j =>
@@ -57,7 +60,7 @@ class JokeList extends Component {
         )
       }),
       () =>
-        window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+        window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
     );
   }
   handleClick() {
@@ -66,26 +69,29 @@ class JokeList extends Component {
   render() {
     if (this.state.loading) {
       return (
-        <div className='JokeList-spinner'>
-          <i className='far fa-8x fa-laugh fa-spin' />
-          <h1 className='JokeList-title'>Loading...</h1>
+        <div className="JokeList-spinner">
+          <i className="far fa-8x fa-laugh fa-spin" />
+          <h1 className="JokeList-title">Loading...</h1>
         </div>
       );
     }
     let jokes = this.state.jokes.sort((a, b) => b.votes - a.votes);
     return (
-      <div className='JokeList'>
-        <div className='JokeList-sidebar'>
-          <h1 className='JokeList-title'>
+      <div className="JokeList">
+        <div className="JokeList-sidebar">
+          <h1 className="JokeList-title">
             <span>Dad</span> Jokes
           </h1>
-          <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
-          <button className='JokeList-getmore' onClick={this.handleClick}>
+          <img
+            alt="icon"
+            src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
+          />
+          <button className="JokeList-getmore" onClick={this.handleClick}>
             Fetch Jokes
           </button>
         </div>
 
-        <div className='JokeList-jokes'>
+        <div className="JokeList-jokes">
           {jokes.map(j => (
             <Joke
               key={j.id}
